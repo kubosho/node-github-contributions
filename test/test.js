fa12956969ba3b8dac71905bfd5f3928eed296a0
kubosho <ta2@o2p.jp>
2014-06-07 21:16:08 +0900

    Add: fetch() test

--- test/test.js
+++ test/test.js
@@ -4,8 +4,24 @@
  * Module dependencies.
  */
 var assert = require('power-assert');
+var contributions = require('../github-contributions.js');
 
-describe('fetch()', function () {
-    describe('', function () {
+describe('github-contributions', function () {
+    describe('fetch()', function () {
+        var status = 0;
+
+        it('Response should be status 200', function (done) {
+            contributions.fetch('kubosho', function(res, statusCode) {
+                done();
+                assert(statusCode === 200);
+            })
+        });
+
+        it('Response should be status 404', function (done) {
+            contributions.fetch('_', function(res, statusCode) {
+                done();
+                assert(statusCode === 404);
+            })
+        });
     });
 });
